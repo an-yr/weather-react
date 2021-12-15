7bca285518c24da546cb0ddd73a789a218028544
Angela Yuri <ang.saito@gmail.com>
2021-12-15 12:09:07 -0300

    updated the default city

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather defaultCity="Rio de Janeiro" />
+      <Weather defaultCity="São Paulo" />
     </div>
   );
 }
